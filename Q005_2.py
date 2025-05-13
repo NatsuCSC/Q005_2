fb18fc7de7901512ad4670790f0cdbdd39c8c879
ナツ <n.otani@cscloud.co.jp>
2025-05-13 12:51:37 +0900

    Improved version

--- Q005_2.py
+++ Q005_2.py
@@ -1,10 +1,8 @@
 H, W = map(int, input().split())
 A = [[*input()] for _ in range(H)]
 B = [[*input()] for _ in range(H)]
-x = sum(A, [])
-y = sum(B, [])
 
-if x.count('@') == y.count('@'):
+if sum(A, []).count('@') == sum(B,[]).count('@'):
     print('Yes')
 else:
     print('No')
